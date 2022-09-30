8cdd35f7921731db71aed6bc25da1702bea5abf1
alexa <alexa@DESKTOP-K114GLU>
2022-09-30 03:21:32 +0300

    Add project files.

---
+++ Rocky/Controllers/ApplicationTypeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+using System.Collections.Generic;
+
+namespace Rocky.Controllers
+{
+    public class ApplicationTypeController : Controller
+    {
+        private ApplicationDBContext _db;
+
+        public ApplicationTypeController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<ApplicationType> objLists = _db.ApplicationType;
+            return View(objLists);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(ApplicationType applicationType)
+        {
+            _db.ApplicationType.Add(applicationType);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}

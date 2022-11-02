f3d47619cf18e13045ec805ab672c4d2fbb23ed1
Alex-Git1347 <sanya.kvitko.1347@gmail.com>
2022-11-02 03:06:36 +0200

    Add functionality updating image of product. Fix validation error.58

--- Rocky/Controllers/ProductController.cs
+++ Rocky/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Rocky.Data;
 using Rocky.Models;
 using Rocky.Models.ViewModels;
@@ -85,11 +86,49 @@ namespace Rocky.Controllers
                     productVM.Product.Image = fileName + extension;
                     _db.Product.Add(productVM.Product);                    
                 }
+                else
+                {
+                    var product = _db.Product.AsNoTracking().FirstOrDefault(x => x.Id == productVM.Product.Id);
+
+                    if (files.Count > 0)
+                    {
+                        var upload = webRootPath + WebConstants.ImagePath;
+                        var fileName = Guid.NewGuid().ToString();
+                        var extension = Path.GetExtension(files[0].FileName);
+
+                        var oldFile = Path.Combine(upload, product.Image);
+
+                        if (System.IO.File.Exists(oldFile))
+                        {
+                            System.IO.File.Delete(oldFile);
+                        }
+
+                        using (var fileStream = new FileStream(Path.Combine(upload, fileName + extension), FileMode.Create))
+                        {
+                            files[0].CopyTo(fileStream);
+                        }
+
+                        productVM.Product.Image = fileName + extension;
+                    }
+                    else
+                    {
+                        productVM.Product.Image = product.Image;
+                    }
+
+                    _db.Product.Update(productVM.Product);
+                }
 
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View();
+
+            productVM.CategorySelectList = _db.Category.Select(i => new SelectListItem
+            {
+                Text = i.Name,
+                Value = i.CategoryId.ToString()
+            });
+
+            return View(productVM);
         }
 
 

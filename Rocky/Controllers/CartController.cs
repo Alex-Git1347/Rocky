6b9643d8f8b62d2ebec25756bfa53cb14756eb86
Alex-Git1347 <sanya.kvitko.1347@gmail.com>
2023-02-27 02:42:19 +0200

    Add functionality and view for summary order.

--- Rocky/Controllers/CartController.cs
+++ Rocky/Controllers/CartController.cs
@@ -2,9 +2,11 @@
 using Microsoft.AspNetCore.Mvc;
 using Rocky.Data;
 using Rocky.Models;
+using Rocky.Models.ViewModels;
 using Rocky.Utility;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 
 namespace Rocky.Controllers
 {
@@ -12,22 +14,48 @@ namespace Rocky.Controllers
     public class CartController : Controller
     {
         private readonly ApplicationDBContext _db;
+        [BindProperty]
+        public ProductUserVM ProductUserVM { get; set; }
 
         public CartController(ApplicationDBContext db)
         {
             _db = db;
         }       
+
         public IActionResult Index()
         {
-            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
-            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WebConstants.SessionCart) != null && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WebConstants.SessionCart).Count() > 0)
-            {
-                shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(WebConstants.SessionCart);
-            }
+            var shoppingCartList = GetShoppingCartList();
             List<int> prodInCart = shoppingCartList.Select(i => i.ProductId).ToList();
             IEnumerable<Product> productList = _db.Product.Where(u => prodInCart.Contains(u.Id));
 
             return View(productList);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Index")]
+        public IActionResult IndexPost()
+        {   
+            return RedirectToAction(nameof(Summary));
+        }
+
+        public IActionResult Summary()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var shoppingCartList = GetShoppingCartList();
+
+            List<int> prodInCart = shoppingCartList.Select(i => i.ProductId).ToList();
+            IEnumerable<Product> productList = _db.Product.Where(u => prodInCart.Contains(u.Id));
+
+            ProductUserVM = new ProductUserVM()
+            {
+                ApplicationUser = _db.ApplicationUser.FirstOrDefault(u => u.Id == claim.Value),
+                ProductList = productList
+            };
+
+            return View(ProductUserVM);
         }
 
         public IActionResult Remove(int id)

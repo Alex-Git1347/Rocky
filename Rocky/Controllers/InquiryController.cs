0ec9c3aa4a5266e66ad5aef3e3ac006829d4d9b6
Alex-Git1347 <sanya.kvitko.1347@gmail.com>
2023-03-28 01:20:06 +0300

    Add controller and view for Inquiry of orders.

---
+++ Rocky/Controllers/InquiryController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.DataAccess.IRepository;
+
+namespace Rocky.Controllers
+{
+    
+    public class InquiryController : Controller
+    {
+        private readonly IInquiryHeaderRepository _inquiryHeaderRepository;
+        private readonly IInquiryDetailRepository _inquiryDetailRepository;
+
+        public InquiryController(IInquiryHeaderRepository inquiryHeaderRepository, IInquiryDetailRepository inquiryDetailRepository)
+        {
+            _inquiryHeaderRepository = inquiryHeaderRepository;
+            _inquiryDetailRepository = inquiryDetailRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
